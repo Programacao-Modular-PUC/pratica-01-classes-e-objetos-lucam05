b490944df6a526e8327f351df84af7cf7cfe7fa1
Luca Moreira Ribeiro Mazala de Araujo <lucamazala10@gmail.com>
2026-03-12 23:16:27 -0300

    Enhance user input validation and output formatting in Main class for Pessoa registration

--- parte2/src/main/java/br/lpm/Main.java
+++ parte2/src/main/java/br/lpm/Main.java
@@ -11,21 +11,22 @@ public class Main {
 
         Pessoa[] p= new Pessoa[10];
 
-        
+        int totalCadastrados = 0;
         for(int i=0; i<p.length; i++){
-        if(i>0){
-            if(p[i-1].getNome().equalsIgnoreCase("Pessoa já cadastrada")){
-                break;
+            System.out.println("--- Cadastro da Pessoa " + (i + 1) + " ---");
+            
+            System.out.println("Digite o nome:");
+            String nome = scanner.nextLine();
+
+            System.out.println("Digite o sobrenome:");
+            String sobrenome = scanner.nextLine();
+        if(i > 0){
+                if(nome.equalsIgnoreCase(p[i-1].getNome()) && sobrenome.equalsIgnoreCase(p[i-1].getSobrenome())){
+                    System.out.println("Pessoa já cadastrada. Encerrando os cadastros...");
+                    break; 
+                }
             }
-        }
-        System.out.println("Digite o nome:");
-        String nome = scanner.nextLine();
-
-        System.out.println("Digite o sobrenome:");
-        String sobrenome = scanner.nextLine();
-
         
-
         System.out.println("Digite a altura:");
         double altura = scanner.nextDouble();
 
@@ -46,10 +47,23 @@ public class Main {
         
         p[i] = new Pessoa(nome, sobrenome, dataNascimento, altura, peso);
         p[i].CalcularIMC();
+        totalCadastrados++;
         
 
         System.out.println("O IMC de " + p[i].getNome() + " " + p[i].getSobrenome() + " é: " + p[i].getImc());
         p[i].InformaObesidade(p[i].getImc());
+    }
+    System.out.println("\n=== RESULTADOS FINAIS ===");
+    for(int i = 0; i < totalCadastrados; i++){
+        String nomeReferencia = p[i].getSobrenome() + ", " + p[i].getNome().toUpperCase();
+
+            System.out.println("Cadastro " + (i + 1) + ":"); 
+            System.out.println("Nome completo: " + p[i].getNome() + " " + p[i].getSobrenome()); 
+            System.out.println("Nome de referência: " + nomeReferencia); 
+            System.out.println("Idade: " + p[i].calcularIdade()); 
+            System.out.println("Peso: " + p[i].getPeso()); 
+            System.out.println("Altura: " + p[i].getAltura()); 
+            System.out.println("IMC: " + String.format("%.2f", p[i].getImc())); 
     }
     
 
